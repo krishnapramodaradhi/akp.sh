1588d28b5e7d01923de80d9c7a0e4fe0c2d96f2d
Krishna Pramod <krishna.pramod2@gmail.com>
2020-08-30 17:53:17 +0530

    Design revamp

--- public/app.js
+++ public/app.js
@@ -1,10 +1,10 @@
 const form = document.querySelector('#shortUrlForm');
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const { url, slug } = event.target;
-  if (!url.value || !url.value.match(/^(https?):\/\/[^\s$.?#].[^\s]*$/gm)) {
-    return;
-  }
-  console.log('here');
-});
+// form.addEventListener('submit', (event) => {
+//   event.preventDefault();
+//   const { url, slug } = event.target;
+//   if (!url.value || !url.value.match(/^(https?):\/\/[^\s$.?#].[^\s]*$/gm)) {
+//     return;
+//   }
+//   console.log('here');
+// });
